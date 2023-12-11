e1f2d155cd357e5b065e067996aeb7dbc1010c4d
Nihuaway <nikitatroshin2305@gmail.com>
2023-12-11 22:30:02 +0300

    little fix in validation handler

--- src/handlers/ValidationHandler/ValidationHandler.ts
+++ src/handlers/ValidationHandler/ValidationHandler.ts
@@ -4,21 +4,25 @@ import {type} from "os";
 
 
 export class ValidationHandler{
-    filter: object;
+    filter: string[];
+    a: number;
 
-    constructor(filter: object) {
+    constructor(filter: string[]) {
         this.filter = filter
+        this.a = 30;
     }
 
     exist(req: Request, res: Response, next: NextFunction){
-        Object.keys(this.filter).map(key => {
-            if(typeof req.body[key] !== typeof key || req.body[key] === undefined){
-                console.log(req.body, key)
-                throw new ErrorHandler(400, `Invalid input data: ${key}`)
-            }
-        })
-
-        next()
+        try{
+            this.filter.map(key => {
+                if(req.body[key] === undefined){
+                    throw new ErrorHandler(400, `Invalid input data: ${key}`)
+                }
+            })
+            next()
+        }catch (e) {
+            next(e)
+        }
     }
 }
 
