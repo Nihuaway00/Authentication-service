7f60c00aca45b6aa02d0e55d47958b85ccbe5a62
Nihuaway <nikitatroshin2305@gmail.com>
2023-12-13 15:58:49 +0300

    small changes in routes

--- src/routes.ts
+++ src/routes.ts
@@ -4,20 +4,27 @@ import {ValidationHandler} from "./handlers/ValidationHandler/ValidationHandler"
 import {NextFunction, Response, Request} from "express-serve-static-core";
 
 
-
-
 export const createRoutes = (router: Router) => {
 	const registrationValidate = new ValidationHandler(['name', 'email', 'password'])
 	const loginValidate = new ValidationHandler(['email', 'password'])
-	const sendRestoreTokenValidate = new ValidationHandler([])
-	const restorePasswordValidate = new ValidationHandler([])
+	const sendRestoreTokenValidate = new ValidationHandler(['email'])
+	const restorePasswordValidate = new ValidationHandler(['password'])
+
 
 	router.post('/login',
 		(req: Request, res: Response, next: NextFunction) => loginValidate.exist(req, res, next),
-		AuthController.login);
+		AuthController.login
+	);
 	router.post('/registration',
 		(req: Request, res: Response, next: NextFunction) => registrationValidate.exist(req, res, next),
-		AuthController.registration);
-	// router.post('/sendRestoreToken', sendRestoreTokenValidate.exist, AuthController.sendRestoreLink);
-	// router.post('/restorePassword', restorePasswordValidate.exist, AuthController.restorePassword);
+		AuthController.registration
+	);
+	router.post('/sendRestoreToken',
+		(req: Request, res: Response, next: NextFunction) => sendRestoreTokenValidate.exist(req, res, next),
+		AuthController.sendRestoreLink
+	);
+	router.post('/restorePassword',
+		(req: Request, res: Response, next: NextFunction) => restorePasswordValidate.exist(req, res, next),
+		AuthController.restorePassword
+	);
 }
